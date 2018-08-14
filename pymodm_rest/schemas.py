249f120380ae861a7a31d40e28c0146020aed66f
artem <lokoArt90@gmail.com>
2018-08-14 19:43:18 +0700

    Improved MongoSchema

--- pymodm_rest/schemas.py
+++ pymodm_rest/schemas.py
@@ -1,6 +1,11 @@
 import coreapi
+from collections import namedtuple
 from rest_framework.schemas import AutoSchema
 from django.utils.six.moves.urllib import parse as urlparse
+
+MethodField = namedtuple('Field',
+                         ['name', 'required', 'location', 'schema', 'description', 'type', 'example', 'methods'])
+MethodField.__new__.__defaults__ = (False, '', None, None, None, None, None)
 
 
 class MongoSchema(AutoSchema):
@@ -10,9 +15,8 @@ class MongoSchema(AutoSchema):
         fields += self.get_pagination_fields(path, method)
         fields += self.get_filter_fields(path, method)
 
-        if method not in ['GET', 'DELETE']:
-            manual_fields = self.get_manual_fields(path, method)
-            fields = self.update_fields(fields, manual_fields)
+        manual_fields = self.get_manual_fields(path, method)
+        fields = self.update_fields(fields, manual_fields)
 
         if fields and any([field.location in ('form', 'body') for field in fields]):
             encoding = self.get_encoding(path, method)
@@ -31,3 +35,11 @@ class MongoSchema(AutoSchema):
             fields=fields,
             description=description
         )
+
+    def get_manual_fields(self, path, method):
+        fields = []
+        for field in self._manual_fields:
+            if field[7] is None or method in field[7]:
+                fields.append(coreapi.Field(field[0], field[1], field[2], field[3], field[4], field[5], field[6]))
+
+        return fields
